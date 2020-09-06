8128ed956bc28353f6242a88fe38bee0879eb998
mostafico <mostafico@yahoo.com>
2020-09-06 20:45:44 +0200

    Added skilliq recyclerview with all its features

--- app/src/main/java/com/example/myapplication/SkillIQRecyclerAdapter.java
+++ app/src/main/java/com/example/myapplication/SkillIQRecyclerAdapter.java
@@ -1,30 +1,53 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class SkillIQRecyclerAdapter extends RecyclerView.Adapter<SkillIQRecyclerAdapter.ViewHolder> {
+    private final LayoutInflater mLayoutInflater;
+    private final Context mContext;
+    private final List<Student> mStudents;
+    public SkillIQRecyclerAdapter(Context context){
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+        mStudents = DataManager.getData();
+    }
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemView = mLayoutInflater.inflate(R.layout.skilliq_card, parent, false);
+        return new SkillIQRecyclerAdapter.ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Student student = mStudents.get(position);
+        holder.tvName.setText(student.getName());
+        holder.tvDetails.setText(student.getScore() + " learning hours, " + student.getCountry() + ".");
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mStudents.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView tvName;
+        public final TextView tvDetails;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvDetails = itemView.findViewById(R.id.tv_details);
         }
     }
 }
--- app/src/main/java/com/example/myapplication/ui/main/SkillFragment.java
+++ app/src/main/java/com/example/myapplication/ui/main/SkillFragment.java
@@ -9,8 +9,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.LearningRecyclerAdapter;
 import com.example.myapplication.R;
+import com.example.myapplication.SkillIQRecyclerAdapter;
 
 public class SkillFragment extends Fragment {
     private int index;
@@ -18,7 +22,12 @@ public class SkillFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+        RecyclerView recyclerView = root.findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        SkillIQRecyclerAdapter recyclerAdapter = new SkillIQRecyclerAdapter(getContext());
 
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
         return root;
     }
 }
