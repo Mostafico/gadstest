d889e627cd70939c189dd554ed2a4dd1c585c241
mostafico <mostafico@yahoo.com>
2020-09-06 18:32:33 +0200

    Deleted Un necessary files of default tabbed activity

--- app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/myapplication/ui/main/SectionsPagerAdapter.java
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.example.myapplication.Frag1;
 import com.example.myapplication.R;
 
 /**
