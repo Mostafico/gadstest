ae1a9b817a488f112e902c4ba368d6db69e467f7
mostafico <mostafico@yahoo.com>
2020-09-06 18:11:56 +0200

    Updated App Name

--- app/src/main/java/com/example/myapplication/ui/main/PageViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/main/PageViewModel.java
@@ -18,8 +18,4 @@ public class PageViewModel extends ViewModel {
     public void setIndex(int index) {
         mIndex.setValue(index);
     }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
