24b5ab2a5924855a7853be52e406e7ce6893bc23
mostafico <mostafico@yahoo.com>
2020-09-06 19:41:39 +0200

    Added student class

---
+++ app/src/main/java/com/example/myapplication/Student.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+public class Student {
+    private String name;
+    private int score;
+    private String country;
+
+    public Student(String name, int score, String country) {
+        this.name = name;
+        this.score = score;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
