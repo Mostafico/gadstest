622b0d4682160fbe05bad85eb86f2cd2fd14c89c
mostafico <mostafico@yahoo.com>
2020-09-08 18:07:20 +0200

    Added toolbar to SubmitActivity

--- app/src/main/java/com/example/myapplication/SubmitActivity.java
+++ app/src/main/java/com/example/myapplication/SubmitActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class SubmitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
