90bfd69d6f2f946046088467b3741bbc02bdde49
mostafico <mostafico@yahoo.com>
2020-09-08 19:09:05 +0200

    Added custom toolbar and removed the old one Added back btn in toolbar

--- app/src/main/java/com/example/myapplication/SubmitActivity.java
+++ app/src/main/java/com/example/myapplication/SubmitActivity.java
@@ -3,14 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SubmitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
+
+        ImageButton backBtn = findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SubmitActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
